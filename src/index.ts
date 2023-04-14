d0be7ddcadd2a1271c924fbbd2d0d5c6cd37f072
tonykevin <hi@tonykevin.com>
2023-04-14 00:54:16 -0500

    typescript - es6: default export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hero as SuperHero } from './classes/Hero'
+import powers from './data/powers'
 
 const Hero = 123
 
 const iroman = new SuperHero('Iroman', 1, 55)
 console.log({ Hero })
 console.log({ iroman })
+
+console.log(powers)
